dd54535b5cc577a4cafc2cd5b534877defa6853f
qiaoshuai123 <51735283+qiaoshuai123@users.noreply.github.com>
2020-08-21 17:06:50 +0800

    update axios

--- axios请求.js
+++ axios请求.js
@@ -5,9 +5,9 @@
 import axios from 'axios'
 
 if (process.env.NODE_ENV === 'development') {
-  // axios.defaults.baseURL = 'http://192.168.1.213:20203'
+  // axios.defaults.baseURL = ''
 } else if (process.env.NODE_ENV === 'production') {
-  axios.defaults.baseURL = 'http://39.100.128.220:20203' // http://192.168.1.213:20203
+  axios.defaults.baseURL = ''
 }
 
 // 添加请求拦截器
@@ -33,10 +33,9 @@ axios.interceptors.response.use((response) => {
   if (response.data.code === -10) {
     localStorage.clear()
     if (process.env.NODE_ENV === 'development') {
-      // axios.defaults.baseURL = 'http://192.168.1.213:20203'
-      window.location.href = 'http://localhost:20204/#/login'
+      window.location.href = ''
     } else if (process.env.NODE_ENV === 'production') {
-      window.location.href = 'http://39.100.128.220:12345/build/index.html#/login'
+      window.location.href = ''
     }
   }
   return response
